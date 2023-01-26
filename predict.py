9da5c04d07c1f6c2673997505157ceba06eb5863
Nick Kao <kobenkao@gmail.com>
2023-01-26 14:32:38 -0800

    remove safety checker

--- predict.py
+++ predict.py
@@ -19,21 +19,14 @@ from diffusers.pipelines.stable_diffusion.safety_checker import (
 
 MODEL_ID = "burnerbaby/sds"
 MODEL_CACHE = "diffusers-cache"
-SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipeline...")
-        safety_checker = StableDiffusionSafetyChecker.from_pretrained(
-            SAFETY_MODEL_ID,
-            cache_dir=MODEL_CACHE,
-            local_files_only=True,
-        )
         self.pipe = StableDiffusionPipeline.from_pretrained(
             MODEL_ID,
-            safety_checker=safety_checker,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
         ).to("cuda")
